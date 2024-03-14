f88c24c043a74b236aa486c5a239f94ef5df4e26
suhailthajudeen <suhail@zoiteckh.com>
2024-03-14 09:16:13 +0530

    latest lang changes

--- NativeSdkTest/ViewController.swift
+++ NativeSdkTest/ViewController.swift
@@ -13,18 +13,28 @@ import Think_research_messenger_SDK_Suhail
 
 class ViewController: UIViewController {
     
-    
+    // staging workflow
     let configuration = ChatBotConfiguration(
         appId: "yB9BJmrcH3bM4CShtMKB5qrw",
         baseUrl: "test.ca.digital-front-door.stg.gcp.trchq.com",
-        originURL: "test.ca.digital-front-door.stg.gcp.trchq.com"
+        originURL: "test.ca.digital-front-door.stg.gcp.trchq.com",
+        lang:"en"
     )
+    
+    // novoscatia
+//    let configuration = ChatBotConfiguration(
+//        appId: "XnA6d2mEejaov78UETAzM5uj",
+//        baseUrl: "test.ca.one-stop-talk.sbx.gcp.trchq.com",
+//        originURL: "app-digitalfrontdoor-dev.apps.ext.novascotia.ca",
+//        lang:"en"
+//    )
+
     
     var chatBotSdk:ChatBotSdk?
     
     override func viewDidLoad()  {
         super.viewDidLoad()
-//        chatBotSdk = ChatBotSdk(configuration: configuration, viewController: self);
+        //        chatBotSdk = ChatBotSdk(configuration: configuration, viewController: self);
     }
     
     @IBAction func btnClicked(_ sender: Any) {
@@ -32,10 +42,10 @@ class ViewController: UIViewController {
         chatBotSdk?.openBot()
     }
     
-
+    
     //
     
-
+    
     
     
 }
