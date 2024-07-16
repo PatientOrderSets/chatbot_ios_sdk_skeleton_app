7e63bd410ec5fa59c553e8e9b628424ac5b5dd45
naseem-thinkresearch <muhammed.naseem-e@thinkresearch.com>
2024-07-16 16:29:08 -0400

    Update pytest_main.py

--- appium_tests/ios/pytest_main.py
+++ appium_tests/ios/pytest_main.py
@@ -5,6 +5,7 @@ from appium.options.ios import XCUITestOptions
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 capabilities = dict(
@@ -15,7 +16,11 @@ capabilities = dict(
     app='com.thinkresearch.app'
 )
 
-# appium_server_url = 'http://hub.browserstack.com/wd/hub'
+app_config = dict(
+    apiKey='${apiKey}',
+    serverURL='${serverURL}'
+)
+
 appium_server_url = 'http://localhost:4723'
 
 @pytest.fixture(scope="function")
@@ -28,7 +33,7 @@ def driver(request):
 
 BUTTON_BACK = (AppiumBy.XPATH, "//XCUIElementTypeApplication[@name=\"ThinkResearch Chatbot\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeButton[1]")
 BUTTON_SELECT_SURVEY = (AppiumBy.ACCESSIBILITY_ID, "Survey")
-BUTTON_FAB = (AppiumBy.XPATH, "//XCUIElementTypeButton")
+BUTTON_FAB = (AppiumBy.XPATH, "//XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeButton[3]")
 BUTTON_NEW_CONVO = (AppiumBy.ACCESSIBILITY_ID, "Start a conversation")
 BUTTON_SELECT_OPTION = (AppiumBy.ACCESSIBILITY_ID, "Select an option")
 BUTTON_SEND = (AppiumBy.XPATH, "//XCUIElementTypeApplication[@name=\"ThinkResearch Chatbot\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeButton[3]")
@@ -39,10 +44,22 @@ OPTION_SURVEY = (AppiumBy.XPATH, "//XCUIElementTypeButton[@name=\"Survey\"]")
 OPTION_ENTER_INPUT = (AppiumBy.XPATH, "//XCUIElementTypeButton[@name=\"Enter input\"]")
 OPTION_ASSIGN_AGENT = (AppiumBy.XPATH, "//XCUIElementTypeButton[@name=\"Assign to agent\"]")
 OPTION_BACK = (AppiumBy.XPATH, "//XCUIElementTypeStaticText[@name=\"Back\"]")
+OPTION_CANCEL = (AppiumBy.XPATH, "//*[contains(@name, 'Cancel') or contains(text(), 'Cancel')]")
 INPUT_FIELD = (AppiumBy.ACCESSIBILITY_ID, "Type your message...")
 SEND_MSG = (AppiumBy.XPATH, "//XCUIElementTypeApplication[@name=\"ThinkResearch Chatbot\"]/XCUIElementTypeWindow[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeOther[2]/XCUIElementTypeButton")
 OPTION_CLOSE = (AppiumBy.XPATH, "//XCUIElementTypeStaticText[@name=\"Close\"]")
 MESSAGE_ASSIGNED_TO = (AppiumBy.XPATH, "//XCUIElementTypeStaticText[contains(@name, \"Assigned to\")]")
+
+BUTTON_SETTINGS =  (AppiumBy.XPATH, "//XCUIElementTypeStaticText[@name=\"Settings\"]")
+SETTINGS_PRESET =   (AppiumBy.XPATH, "//XCUIElementTypeScrollView/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeTextField")
+SETTINGS_SELECT_PROD = (AppiumBy.XPATH, "//XCUIElementTypePickerWheel[@value=\"Other\"] ")
+SETTINGS_BUTTON_SAVE = (AppiumBy.XPATH, "//XCUIElementTypeStaticText[@name=\"Save\"]")
+SETTINGS_TOGGLE = (AppiumBy.XPATH, "//XCUIElementTypeSwitch")
+SETTINGS_BUTTON_BACK = (AppiumBy.ACCESSIBILITY_ID, "Back")
+SETTINGS_APP_ID = (AppiumBy.XPATH, "(//XCUIElementTypeTextField)[2]")
+SETTINGS_ORIGIN = (AppiumBy.XPATH, "(//XCUIElementTypeTextField)[3]")
+SETTINGS_BASE_URL = (AppiumBy.XPATH, "(//XCUIElementTypeTextField)[4]")
+SETTINGS_SELECT_LANGUAGE = (AppiumBy.XPATH, "(//XCUIElementTypeTextField)[5]")
 
 
 class TestAppium:
@@ -59,50 +76,60 @@ class TestAppium:
         element = self.wait.until(EC.presence_of_element_located(locator))
         assert element.is_displayed()
 
-    def test_new_conversation(self):
-        self.click(BUTTON_FAB)
-        self.assert_visible(BUTTON_NEED_CARE)
-    
-    # def test_formatted_msg(self):
-    #     self.click(BUTTON_FAB)
-    #     self.assert_visible(BUTTON_NEED_CARE)
-    #     self.click(BUTTON_BACK)
-    #     self.click(BUTTON_NEW_CONVO)
-    #     self.click(BUTTON_SELECT_OPTION)
-    #     self.click(OPTION_FORMATTED_MSG)
-    #     self.click(BUTTON_SEND)
-    #     self.assert_visible(OPTION_BACK)
+    def tap_by_coordinates(self, x, y):
+        action = self.driver.create_webdriver_action()
+        action.pointer_action.pointer_down(x, y)
+        action.pointer_action.pointer_up()
+        action.perform()
 
-    # def test_survey(self):
-    #     self.click(BUTTON_FAB)
-    #     self.assert_visible(BUTTON_NEED_CARE)
-    #     self.click(BUTTON_BACK)
-    #     self.click(BUTTON_NEW_CONVO)
-    #     self.click(BUTTON_SELECT_OPTION)
-    #     self.click(BUTTON_SELECT_SURVEY)
-    #     self.click(BUTTON_SEND)
-    #     self.assert_visible(BUTTON_START_SURVEY)
-        
-    # def test_enter_input(self):
-    #     self.click(BUTTON_FAB)
-    #     self.assert_visible(BUTTON_NEED_CARE)
-    #     self.click(BUTTON_BACK)
-    #     self.click(BUTTON_NEW_CONVO)
-    #     self.click(BUTTON_SELECT_OPTION)
-    #     self.click(OPTION_ENTER_INPUT)
-    #     self.click(BUTTON_SEND)
-    #     self.click(INPUT_FIELD)
-    #     input_field = self.driver.find_element(*INPUT_FIELD)
-    #     input_field.send_keys("hello")
-    #     self.click(SEND_MSG)
-    #     self.assert_visible(OPTION_CLOSE)
-    
-    # def test_assign_agent(self):
-    #     self.click(BUTTON_FAB)
-    #     self.assert_visible(BUTTON_NEED_CARE)
-    #     self.click(BUTTON_BACK)
-    #     self.click(BUTTON_NEW_CONVO)
-    #     self.click(BUTTON_SELECT_OPTION)
-    #     self.click(OPTION_ASSIGN_AGENT)
-    #     self.click(BUTTON_SEND)
-    #     self.assert_visible(MESSAGE_ASSIGNED_TO)
+    def select_settings(self):
+        self.click(BUTTON_SETTINGS)
+        self.click(SETTINGS_PRESET)
+        self.click(SETTINGS_SELECT_PROD)
+        element = self.wait.until(EC.presence_of_element_located(SETTINGS_SELECT_PROD))
+        element.send_keys("Production")
+        self.click(SETTINGS_BUTTON_SAVE)
+        self.click(SETTINGS_TOGGLE)
+        self.click(SETTINGS_BUTTON_BACK)
+
+    def enter_settings(self):
+        self.click(BUTTON_SETTINGS)
+
+        input_box = self.wait.until(EC.presence_of_element_located(SETTINGS_APP_ID))
+        input_box.send_keys(app_config['apiKey'])
+
+        input_box = self.wait.until(EC.presence_of_element_located(SETTINGS_ORIGIN))
+        input_box.send_keys(app_config['serverURL'])
+
+        input_box = self.wait.until(EC.presence_of_element_located(SETTINGS_BASE_URL))
+        input_box.send_keys(app_config['serverURL'])
+
+        self.click(SETTINGS_SELECT_LANGUAGE)
+        element = self.wait.until(EC.presence_of_element_located(SETTINGS_SELECT_LANGUAGE))
+        element.send_keys("en")
+
+        self.click(SETTINGS_BUTTON_SAVE)
+        self.click(SETTINGS_TOGGLE)
+        self.click(SETTINGS_BUTTON_BACK)
+
+    def test_smoke(self):
+        self.assert_visible(BUTTON_FAB)
+
+    def test_settings(self):
+        self.click(BUTTON_SETTINGS)
+        self.assert_visible(SETTINGS_BUTTON_SAVE)
+
+    def test_save_settings(self):
+        self.enter_settings()
+        self.assert_visible(BUTTON_FAB)
+
+    def test_new_conversation(self):
+        self.enter_settings()
+        self.click(BUTTON_FAB)
+        self.assert_visible(BUTTON_SELECT_OPTION)
+
+    def test_option_list(self):
+        self.enter_settings()
+        self.click(BUTTON_FAB)
+        self.click(BUTTON_SELECT_OPTION)
+        self.assert_visible(OPTION_CANCEL)
